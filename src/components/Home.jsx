e976d58304928558fd18186f893e81c13078adbf
Hay Chief! <abiramofficial002@gmail.com>
2025-02-02 19:10:34 +0530

    added some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import BGSVG from '../assets/bg.svg'
 
 const Home = () => {
 
@@ -28,24 +29,31 @@ const Home = () => {
     }
 
     return (
-        <section className='bg-gray-300 h-[100vh]' >
+        <section 
+        style={{
+            backgroundImage: `url(${BGSVG})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+        }}
+        >
             <div className='w-full fixed' >
-                <nav className=" mx-auto my-10 w-11/12 md:w-3/4 max-w-screen-lg px-4 py-2 bg-white shadow-md rounded-md lg:px-8 lg:py-3">
+                <nav className=" mx-auto my-10 w-11/12 md:w-3/4 max-w-screen-lg px-4 py-2 bg-gray-800 shadow-lg shadow-slate-500 rounded-md lg:px-8 lg:py-3 ">
                     <div className="container flex flex-col md:flex-row flex-wrap items-center justify-between mx-auto text-slate-800">
-                        <p className="md:mr-4 text-center py-1.5 text-base lg:text-xl text-slate-800 font-semibold">
+                        <p className="md:mr-4 text-center py-1.5 text-base lg:text-xl text-gray-200 font-semibold">
                             Image Generator
                         </p>
 
                         <form onSubmit={handleForm} className="items-center lg:flex-none flex-1 lg:w-3/4 gap-x-2 w-full flex">
                             <div className="w-full min-w-[200px]">
                                 <div className="relative">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="absolute w-5 h-5 top-2.5 left-2.5 text-white">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                     </svg>
-                                    <input required onChange={(e) => { setPrompt(e.target.value) }} type="text" className="w-full pl-10 pr-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-600 text-sm border border-slate-200 rounded-md transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Enter your prompt ..." />
+                                    <input required onChange={(e) => { setPrompt(e.target.value) }} type="text" className="w-full pl-10 pr-3 py-2 bg-transparent placeholder:text-slate-400 text-white text-sm border border-slate-200 rounded-md transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Enter your prompt ..." />
                                 </div>
                             </div>
-                            <button type='submit' className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                            <button type='submit' className="rounded-md bg-gray-100 py-2 px-4 border border-transparent text-center text-sm text-gray-800 font-semibold transition-all shadow-md hover:shadow-lg focus:bg-gray-300 focus:shadow-none active:bg-gray-400 hover:bg-gray-300 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                                 Create
                             </button>
                         </form>
@@ -54,8 +62,8 @@ const Home = () => {
             </div>
             <main className=' flex justify-center items-center relative top-40' >
                 {
-                    loading ? 'Loading...' : ((!loading && image) && <div className=''>
-                        <img className='h-96 w-96 rounded-md' src={image} alt="Generated-img" />
+                    loading ? <h2 className='text-2xl' >Loading...</h2> : ((!loading && image) && <div className=''>
+                        <img className='h-96 w-96 shadow-2xl shadow-black rounded-md' src={image} alt="Generated-img" />
                     </div>)
                 }
             </main>
